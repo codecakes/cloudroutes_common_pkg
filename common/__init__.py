c1c79c34f4450ff45d0616710e2ae52bbd484d43
codecakes <codecakes@users.noreply.github.com>
2015-04-07 21:19:32 +0530

    Update __init__.py
    adding current dir path wherever this pkg exists to sys path if its not already present.

--- common/__init__.py
+++ common/__init__.py
@@ -3,7 +3,8 @@ import sys
 import inspect
 
 cur_path = os.path.dirname(os.path.realpath(os.path.abspath(inspect.getfile(inspect.currentframe()))))
-sys.path.insert(1, cur_path)
-print "common pkg dir name {} added to sys path".format(sys.path[cur_path])
+if cur_path not in sys.path:
+	sys.path.insert(1, cur_path)
+	print "common pkg dir name {} added to sys path".format(sys.path[cur_path])
 
 
